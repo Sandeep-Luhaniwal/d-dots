fc41dbfedf9857a0a82a5bd6c654a19a7a1987af
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-12-04 16:46:13 +0530

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-// hlo
+// VIDEO JS START
+let vid = document.getElementById("myVideo");
+let playButton = document.getElementById("playVideo");
+function playVid() {
+    vid.play();
+    vid.controls = true; // Show controls when video starts playing
+    playButton.classList.add("d-none");
+}
+vid.addEventListener("play", function () {
+    vid.controls = true; // Show controls when video starts playing
+    playButton.classList.add("d-none");
+});
+vid.addEventListener("pause", function () {
+    vid.controls = false; // Hide controls when video is paused
+    playButton.classList.remove("d-none");
+});
+
+// VIDEO JS END
